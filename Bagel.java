ecf2a83abb2fa0aa44115bb2dd0c3d0c9cb4a8a5
Jeff Ambrose <jscottambrose@gmail.com>
2016-11-03 20:04:19 -0700

    Upload

---
+++ Bagel.java
@@ -0,0 +1,13 @@
+/**
+   This program creates an instance of the OrderCalculatorGUI class
+   which displays the GUI for the Brandi's Bagel House application.
+*/
+package bagelhouse;
+
+public class Bagel
+{
+   public static void main(String[] args)
+   {
+      new mainMenu();
+   }
+}
